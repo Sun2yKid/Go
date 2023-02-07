6f8dbff640f1c4d0326924736c7f182de3f7b6b0
Sun2yKid <zhonghua00700@gmail.com>
2023-02-07 21:03:49 +0800

    little

--- Module-Import/myremotepackage/mypackage/mypackage.go
+++ Module-Import/myremotepackage/mypackage/mypackage.go
@@ -3,5 +3,6 @@ package mypackage
 import "fmt"
 
 func New() {
-	fmt.Println("github.com/Sun2yKid/Go/Module-Import/myremotepackage/mypackage.New")
+	fmt.Println("--------------------------------------------------------------------")
+	fmt.Println("github.com/Sun2yKid/Go/Module-Import/myremotepackage/mypackage.New()")
 }
